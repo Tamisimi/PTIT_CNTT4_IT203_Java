668538872aab387889d62957d767b798124cfdf1
Tamisimi <alexmd0107@gmail.com>
2026-02-02 09:10:24 +0700

    Update BAI01.java

--- SESSION06/BAI01/BAI01.java
+++ SESSION06/BAI01/BAI01.java
@@ -22,8 +22,8 @@ public class BAI01 {
     }
 
     public static void main(String[] args) {
-        SS06_01 sv1 = new SS06_01("SV001", "Nguyễn Văn A", 2004, 8.2);
-        SS06_01 sv2 = new SS06_01("SV002", "Trần Thị B", 2003, 7.6);
+        BAI01 sv1 = new BAI01("SV001", "Nguyễn Văn A", 2004, 8.2);
+        BAI01 sv2 = new BAI01("SV002", "Trần Thị B", 2003, 7.6);
 
         sv1.hienThiThongTin();
         sv2.hienThiThongTin();
