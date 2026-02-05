f25678327fc776546ce377d6813c7e800bbbf6d0
Tamisimi <alexmd0107@gmail.com>
2026-02-06 01:24:43 +0700

    Create BAI02.java

---
+++ SESSION09/BAI02/BAI02.java
@@ -0,0 +1,11 @@
+package SESSION09.BAI02;
+
+public class BAI02 {
+    public static void main(String[] args) {
+        Animal s1 = new Dog();
+        Animal s2 = new Cat();
+
+        s1.sound();
+        s2.sound();
+    }
+}
