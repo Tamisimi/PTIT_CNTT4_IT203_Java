e87385647a337566d50b8b4efd04151bdb4eff2d
Tamisimi <alexmd0107@gmail.com>
2026-02-06 01:25:50 +0700

    Create BAI03.java

---
+++ SESSION09/BAI03/BAI03.java
@@ -0,0 +1,8 @@
+package SESSION09.BAI03;
+
+public class BAI03 {
+    public static void main(String[] args) {
+        Manager m = new Manager("Nguyễn Văn B", 15000000, "IT");
+        m.display();
+    }
+}
