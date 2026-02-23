4b6d519dd52f2ebceb9f1c0289838d42f388bc3f
Tamisimi <alexmd0107@gmail.com>
2026-02-24 02:11:43 +0700

    Update Main.java

--- SESSION010/BAI06/Main.java
+++ SESSION010/BAI06/Main.java
@@ -12,19 +12,13 @@ public class Main {
         list.add(new Product("Chuột", 500));
         list.add(new Product("Bàn phím", 1000));
 
-        //Cách 1:
         Collections.sort(list, new Comparator<Product>() {
             @Override
             public int compare(Product p1, Product p2) {
-                // Lấy giá đứa trước trừ giá đứa sau.
-                // Nếu ra số âm -> p1 rẻ hơn -> Đứng trước.
                 return p1.price - p2.price;
             }
         });
 
-        // Cách 2:
-        // Dịch nghĩa: Đưa cho tôi 2 sản phẩm (p1, p2),
-        // mũi tên (->) chỉ ra hành động tôi muốn làm là: lấy tên chúng nó so sánh với nhau.
         System.out.println("--- Sắp xếp theo Giá (Anonymous Class) ---");
         for (Product p : list) { System.out.println(p); }
         Collections.sort(list, (p1, p2) -> p1.name.compareTo(p2.name));
