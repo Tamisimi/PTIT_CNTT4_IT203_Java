cd1627a60cc5a4abf9591873fe877db45200852e
Tamisimi <alexmd0107@gmail.com>
2026-02-24 02:05:34 +0700

    Create Main.java

---
+++ SESSION010/BAI02/Main.java
@@ -0,0 +1,11 @@
+package SESSION10.BAI02;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle v1 = new Car("VinFast");
+        Vehicle v2 = new Bicycle("Martin");
+
+        v1.move();
+        v2.move();
+    }
+}
